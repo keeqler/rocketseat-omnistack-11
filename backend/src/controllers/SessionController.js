4e0661dfa618764329d0fa3f38b3951c5525e563
Keepler <arturnunespe@gmail.com>
2020-04-04 06:39:54 -0300

    Install eslint packages and apply

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -4,17 +4,14 @@ class SessionController {
   async store(request, response) {
     const { id } = request.body;
 
-    const ong = await db('ongs')
-      .where({ id })
-      .select('name')
-      .first();
+    const ong = await db('ongs').where({ id }).select('name').first();
 
     if (!ong)
       return response
         .status(401)
         .send({ error: 'Could not find an ONG with that ID' });
 
-    response.send(ong);
+    return response.send(ong);
   }
 }
 
